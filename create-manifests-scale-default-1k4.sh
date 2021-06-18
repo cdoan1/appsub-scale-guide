08cc401a4b6de2e13489933619f5cfc62a72c1e9
Chris Doan <cdoan@redhat.com>
2021-06-18 08:54:43 -0500

    add multiple appsub of many resources

---
+++ create-manifests-scale-default-1k4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#network_type="OpenShiftSDN"
+network_type="OVNKubernetes"
+
+generate_manifest_yamls() {
+  
+  local yaml_dir="1k-scale-default-4"
+  mkdir -p "$yaml_dir/scale$1"
+  local cluster_name="scale-default-4"
+
+  echo "====== Generating manifests for $cluster_name  ======"
+  sed -e s/\{\{NAMESPACE\}\}/"$cluster_name"/g \
+    -e s/\{\{INDEX\}\}/"$1"/g \
+    templates/cm-scale-default.template.yaml >"$yaml_dir"/scale$1/100-cm.yaml
+
+}
+
+for i in {1..1000}
+do
+  foo=$(printf "%05d" $i)
+  generate_manifest_yamls "$foo"
+done
